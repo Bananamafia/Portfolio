9eb875fb1a560328d7436c109a0090e9677090a9
Maximilian Kiefer <maximilian.kiefer@yahoo.de>
2020-07-12 10:10:46 +0200

    Added seed data to the DB.

---
+++ PortfolioWebApp/Models/SeedData.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using PortfolioWebApp.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioWebApp.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new PortfolioWebAppContext(serviceProvider.GetRequiredService<DbContextOptions<PortfolioWebAppContext>>()))
+            {
+                if (context.Project.Any())
+                {
+                    return;
+                }
+                else
+                {
+                    context.Project.AddRange(
+                        new Project
+                        {
+                            Title = "Conway's Game of Life",
+                            StartDate = DateTime.Parse("2020-06-01"),
+                            EndDate = DateTime.Parse("2020-07-01"),
+                            Description = "Created a simulation of population of cells with the rules of 'Conway's Game of Life'"
+                        },
+
+                        new Project
+                        {
+                            Title = "Digital Contact Book",
+                            StartDate = DateTime.Parse("2020-04-01"),
+                            EndDate = DateTime.Parse("2020-05-01"),
+                            Description = "Created a contact Book in WPF, which can add, cdit, celete and filter contacts."
+                        });
+
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
