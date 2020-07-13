1cf280d458213c5d845d449e5c4802f0bb1941e7
Maximilian Kiefer <maximilian.kiefer@yahoo.de>
2020-07-13 17:26:02 +0200

    Made few changes in Layout and setting up a ProjectoverviewPage

---
+++ PortfolioWebApp/Pages/Projektübersicht.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortfolioWebApp.Pages
+{
+    public class ProjektübersichtModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
