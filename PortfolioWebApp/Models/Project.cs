e8114d596c5fb8292229700b81c335b809ca803c
Maximilian Kiefer <maximilian.kiefer@yahoo.de>
2020-07-18 13:18:56 +0200

    minor Layout changes

--- PortfolioWebApp/Models/Project.cs
+++ PortfolioWebApp/Models/Project.cs
@@ -12,6 +12,7 @@ namespace PortfolioWebApp.Models
         public string Topic { get; set; } = "Sonstiges";
         public string Title { get; set; } = "Sample Project";
         public string Image { get; set; } = "/Images/SampleImage.png";
+        public string Thumbnail { get; set; } = "/Images/Thumbnails/SampleImage.png";
         public DateTime StartingDate { get; set; } = new DateTime(1995, 12, 15);
         public DateTime EndingDate { get; set; } = DateTime.Now;
         public string Description { get; set; } = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.";
