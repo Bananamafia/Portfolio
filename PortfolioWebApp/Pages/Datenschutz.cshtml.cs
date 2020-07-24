152697eca724f3b85d6b4785593613593bbff0f0
Maximilian Kiefer <maximilian.kiefer@yahoo.de>
2020-07-24 18:04:10 +0200

    Added Impressum and Datapolicy

---
+++ PortfolioWebApp/Pages/Datenschutz.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PortfolioWebApp.Pages
+{
+    public class DatenschutzModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
